522ef7ecc47d40674ebda436b75ba647a7a907f9
Afz-Mawardi <fitrinarti341@gmail.com>
2024-11-02 21:35:53 +0700

    Add files via upload

---
+++ Latihan_18.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author user
+ * NAMA     : Afsani Wahyu Mawardi
+ * KELAS    : TI RegPagi
+ * NIM      : 23215058
+ * Dekskipsi 
+ * Progam   : Progam ini untuk menampilkan Operator, While
+ *          Kelipatan
+ */
+public class Latihan_18 {
+     public static void main(String[] args) {
+        double i = 3.5;
+        while (i <= 35.0) {
+            System.out.println(i);
+            i += 3.5;
+        }
+        System.out.println("BUILD S");
+    }
+}
